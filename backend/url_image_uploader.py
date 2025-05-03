99753cddac421bda0ba00b1bfa8329fbee67d758
siscubl04 <siscubadial@gmail.com>
2025-05-03 03:15:17 +0200

    Functionality  and mock usecase to upload images to a url  using Firebase

---
+++ backend/url_image_uploader.py
@@ -0,0 +1,22 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+import uuid
+
+class FirebaseStorageManager:
+    def __init__(self, cred_path, bucket_name):
+        # Inicializar la aplicación de Firebase con las credenciales
+        cred = credentials.Certificate(cred_path)
+        firebase_admin.initialize_app(cred, {'storageBucket': bucket_name})
+        self.bucket = storage.bucket()
+
+    def upload_image(self, image_path):
+        # Generar un nombre único para la imagen
+        image_name = f"images/{uuid.uuid4()}.jpg"
+        # Crear una referencia al blob en el bucket
+        blob = self.bucket.blob(image_name)
+        # Subir la imagen al blob
+        blob.upload_from_filename(image_path)
+        blob.make_public() # Hacer el blob público
+        # Obtener la URL pública de la imagen
+        image_url = blob.public_url
+        return image_url
