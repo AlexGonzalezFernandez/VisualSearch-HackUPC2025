93133193f6623cf4899a9ff8f8c718c8f0b3d3d8
msantama <msantama@pic.es>
2025-05-03 19:15:28 +0200

    Cutre-session

--- backend/auth.py
+++ backend/auth.py
@@ -1,9 +1,37 @@
-from config import *
+from backend.config import *
+import json
 from keycloak import KeycloakOpenID
 
-keycloak_openid = KeycloakOpenID(
-    server_url=KEYCLOAK_SERVER_URL,
-    client_id=KEYCLOAK_CLIENT_ID,
-    realm_name=KEYCLOAK_REALM,
-    client_secret_key=KEYCLOAK_CLIENT_SECRET,
-)
+# Load Keycloak configuration from JSON file
+with open('config/marc_config.json', 'r') as config_file:
+    config = json.load(config_file)
+
+KEYCLOAK_SERVER_URL = config.get("server_url")
+KEYCLOAK_CLIENT_ID = config.get("client_id")
+KEYCLOAK_REALM = config.get("realm_name")
+KEYCLOAK_CLIENT_SECRET = config.get("client_secret")
+
+def use_kc_openid():
+    keycloak_openid = KeycloakOpenID(
+        server_url=KEYCLOAK_SERVER_URL,
+        client_id=KEYCLOAK_CLIENT_ID,
+        realm_name=KEYCLOAK_REALM,
+        client_secret_key=KEYCLOAK_CLIENT_SECRET,
+    )
+    return keycloak_openid
+
+def get_keycloak_tokens(code, realm, client_id, secret, redirect_uri):
+    # Example implementation to use the parameters
+    keycloak_openid = use_kc_openid()
+    token_data = keycloak_openid.token(grant_type="authorization_code", code=code, redirect_uri=redirect_uri)
+    refresh_token=token_data.get("refresh_token")
+    access_token = token_data.get("access_token")
+    # ID token is also verified in the decode_token method
+    id_decoded = keycloak_openid.decode_token(token_data.get("id_token"))
+
+    
+    return {
+        "access_token": access_token,
+        "id_token": id_decoded,
+        "refresh_token":refresh_token,
+    }
--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,14 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import JSONResponse
 import shutil
-
+import json
 from inditex_api import InditexVisualSearchAPI
 from url_image_uploader import FirebaseStorageManager
+from utils import generate_login_url
+from auth import get_keycloak_tokens
+import secrets
+
+SESSION = {}
 
 app = FastAPI()
 
@@ -13,6 +18,15 @@ inditex_api = InditexVisualSearchAPI()
 cred_path = 'config/visualsearchhackupc-firebase-adminsdk-fbsvc-9483e6d9c2.json'
 bucket_name = 'visualsearchhackupc.firebasestorage.app'
 uploader = FirebaseStorageManager(cred_path, bucket_name)
+
+with open('../url.txt', 'r') as file:
+    URL_PREFIX = file.read().strip()
+
+with open('config/marc_keys.json', 'r') as file:
+    keys = json.load(file)
+    CLIENT_ID = keys.get("client_id")
+    KEYCLOAK_REALM = keys.get("realm_name")
+    secret = keys
 
 @app.post("/upload/")
 async def upload_image(file: UploadFile = File(...)):
@@ -42,34 +56,65 @@ async def home(request: Request):
     """
     Simple home, shows a welcome message with login and register links.
     """
-    base_url = f"{request.url.scheme}://{request.headers['host']}"
     return {
         "message": "Welcome to the Visual Search API!",
         "actions": {
-            "login": f"{base_url}/home/login",
-            "register": f"{base_url}/home/register"
+            "login": f"{URL_PREFIX}/home/login",
+            "register": f"{URL_PREFIX}/home/register"
         }
     }
 
 
+
 @app.get("/home/login")
 async def login():
+    """Login logic page."""
+    state = secrets.token_urlsafe(16)
+    SESSION['state'] = state
+    login_url = generate_login_url(state=state)
+    return {"login_url": login_url}
+
+
+
+@app.get("/home/login/callback")
+async def login_callback(request: Request):
     """
-    Simple home, shows a welcome, login and register message.
+    Keycloak callback logic page.
     """
-    return {"message": "Welcome to the Visual Search API! Please login or register."}
+    #Get the state
+    received_state = request.query_params.get("state")
+    stored_state = SESSION.get("state")  # Retrieve & remove state
+    SESSION.pop("state", None)
+    code = request.query_params.get("code")
+    #Check the state
+    if received_state != stored_state or not code:
+        return JSONResponse("Invalid user", status_code=404)
+    
+    tokens = get_keycloak_tokens(code, KEYCLOAK_REALM, CLIENT_ID, secret, f"{URL_PREFIX}/callback")
+
+    id_token = tokens.get('id_token')
+    access_token = tokens.get('access_token')
+    refresh_token = tokens.get('refresh_token')
+
+    # Store user details in the session
+    request.session["authenticated"] = True
+    request.session["username"] = id_token.get("preferred_username")
+    request.session["id_token"] = id_token
+    request.session["access_token"] = access_token
+    request.session["refresh_token"] = refresh_token
+
+    return JSONResponse("User logged in successfully", status_code=200)
+
+
+
+
 @app.get("/home/register")
 async def register():
     """
     Simple home, shows a welcome, login and register message.
     """
     return {"message": "Welcome to the Visual Search API! Please login or register."}
-@app.get("/home/login/callback")
-async def login_callback():
-    """
-    Simple home, shows a welcome, login and register message.
-    """
-    return {"message": "Welcome to the Visual Search API! Please login or register."}
+
 @app.get("/home/logout")
 async def logout():
     """
